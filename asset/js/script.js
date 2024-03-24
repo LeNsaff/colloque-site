de6a3f1f254cecb6d8a6b455a5b8baa256f3c8b8
LeNsaff <34993921+LeNsaff@users.noreply.github.com>
2024-03-24 14:17:42 +0100

    4btn

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,10 +2,11 @@ var popup = document.getElementById('popup');
 var logbtn = document.getElementById('logbtn')
 var popupc = document.getElementById('popupc')
 
-var buttonIds = ["button1", "button2", "button3"];
+var buttonIds = ["button1", "button2", "button3", "button4"];
 var main1 = document.getElementById("main1");
 var main2 = document.getElementById("main2");
 var main3 = document.getElementById("main3");
+var main4 = document.getElementById("main4");
 
 function defile() {
     var buttonId = this.id;
@@ -15,14 +16,22 @@ function defile() {
         main1.style.display = 'flex';
         main2.style.display = 'none';
         main3.style.display = 'none';
+        main4.style.display = 'none';
     } else if (buttonId === "button2") {
         main2.style.display = 'flex';
         main1.style.display = 'none';
         main3.style.display = 'none';
+        main4.style.display = 'none';
     } else if (buttonId === "button3") {
         main3.style.display = 'flex';
         main2.style.display = 'none';
         main1.style.display = 'none';
+        main4.style.display = 'none';
+    }else if (buttonId === "button4") {
+        main4.style.display = 'flex';
+        main2.style.display = 'none';
+        main1.style.display = 'none';
+        main3.style.display = 'none';
     }
 }
 
@@ -30,34 +39,3 @@ function defile() {
 buttonIds.forEach(function(buttonId) {
     document.getElementById(buttonId).addEventListener("click", defile);
 });
-
-function pop() {
-    if (popup) {
-        popup.style.display = 'flex';
-        console.log("Element display:", popup.style.display);
-    } else {
-        console.error("Popup element not found!");
-    }
-}
-
-function popd() {
-    if (popup) {
-        popup.style.display = 'none';
-        console.log("Element display:", popup.style.display);
-    } else {
-        console.error("Popup element not found!");
-    }
-}
-
-// Add event listener to the button if logbtn is defined
-if (logbtn) {
-    logbtn.addEventListener("click", pop);
-} else {
-    console.error("Button element not found!");
-}
-
-if (popupc) {
-    popupc.addEventListener("click", popd);
-} else {
-    console.error("Button element not found!");
-}
